6be16168b43563987a3f0cae3b5e215b1bcc2f93
vithor97 <vitorsantos27@hotmail.com>
2020-05-27 16:58:58 -0300

    teste

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
       offset: '60px;'
     });
 
-       /* Scroll on buttons */
+       /* Scroll on buttons teste */
     $('.js--scroll-to-plans').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
      });
